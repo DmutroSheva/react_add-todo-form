9e55fedc3927a5e06a15855b8d5f9f206ee1d45e
DmutroSheva <shevche3@gmail.com>
2025-05-13 17:44:28 +0300

    add task solution

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,12 @@ const initialTodos: Todo[] = todosFromServer.map(todo => {
   };
 });
 
-const getNewTodoId = (todos: Todo[]) => {
-  const maxId = Math.max(...todos.map(todo => todo.id));
+const getNewTodoId = (todos: Todo[]): number => {
+  if (todos.length === 0) {
+    return 1;
+  }
+
+  const maxId: number = Math.max(...todos.map(todo => todo.id));
 
   return maxId + 1;
 };
